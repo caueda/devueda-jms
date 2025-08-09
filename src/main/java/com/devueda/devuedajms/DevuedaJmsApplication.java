235b7799e47d8c51f661372d918c07bb8cd02e02
Carlos R. Ueda <carlos.ueda@gmail.com>
2025-08-09 07:03:27 -0300

    first commit

---
+++ src/main/java/com/devueda/devuedajms/DevuedaJmsApplication.java
@@ -0,0 +1,13 @@
+package com.devueda.devuedajms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevuedaJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevuedaJmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devueda/devuedajms/DevuedaJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devueda.devuedajms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevuedaJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
